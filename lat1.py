187187ea80857a6ecfb16ec9121b2481929cbe8f
felieesya <rendrarim@gmail.co~>
2024-02-17 16:50:14 +0900

    v4

--- lat1.py
+++ lat1.py
@@ -5,8 +5,8 @@ import pandas as pd
 st.write("Elzy Tabel Coding")
 df = pd.DataFrame({
     'No' : [1, 2, 3, 4, 5, 6],
-    'Nama' : ['Sowoon', 'Yuju', 'Yerin', 'Eunha', 'Sinb', 'Umji'],
-    'Tinggi' : [173, 170, 168, 163, 167, 164]
+    'Nama' : ['Nurul', 'Nadia', 'Vino', 'Zahwa'],
+    'Tinggi' : [98, 90, 83, 92]
 })
 
 df
