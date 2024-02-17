bf99467fe74a5e3c84b99386f681701cd926eda4
felieesya <rendrarim@gmail.co~>
2024-02-17 16:19:38 +0900

    v2

--- lat1.py
+++ lat1.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+
+st.write("Elzy Tabel Coding")
 df = pd.DataFrame({
     'No' : [1, 2, 3, 4],
     'Nama' : ['Nurul', 'Nadia', 'Vino', 'Zahwa'],
