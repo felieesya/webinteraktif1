54a31e164ac088135b22211fd1ce1d1827c4d08f
felieesya <rendrarim@gmail.co~>
2024-03-02 11:07:20 +0900

    v7

--- lat1.py
+++ lat1.py
@@ -1,12 +1,34 @@
 import streamlit as st
-import pandas as pd
+#import pandas as pd
 #import matplotlib.pyplot as plt
 
-st.write("List Sahabat")
-df = pd.DataFrame({
-    'No' : [1, 2, 3, 4,],
-    'Nama' : ['KK', 'UU', 'OO', 'II'],
-    'Rating' : [8, 9, 5, 6]
-})
+# st.write("List Sahabat")
+# df = pd.DataFrame({
+#     'No' : [1, 2, 3, 4,],
+#     'Nama' : ['KK', 'UU', 'OO', 'II'],
+#     'Rating' : [8, 9, 5, 6]
+# })
 
-df
+# df
+
+def page_1():
+    st.title("Halaman 1")
+    st.write('Halaman ini digunakan untuk Intro')
+def page_2():
+    st.title("Halaman 2")
+    st.write('Halaman ini digunakan untuk Menampilkan Youtube')
+def page_3():
+    st.title("Halaman 3")
+    st.write('Halaman ini digunakan untuk Menampilkan Rumus Matematika')
+
+PAGES = {
+    "Page 1" : page_1,
+    "Page 2" : page_2,
+    "Page 3" : page_3
+}
+
+page = st.sidebar.radio("Halaman", list(PAGES.keys()))
+PAGES[page]()
+
+
+
