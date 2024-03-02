4e3a7c56ffd5c6cd8a72004790cc6f3b5777707d
felieesya <rendrarim@gmail.co~>
2024-03-02 10:06:23 +0900

    v6

--- lat1.py
+++ lat1.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 #import matplotlib.pyplot as plt
 
-st.write("Kesukaan")
+st.write("List Sahabat")
 df = pd.DataFrame({
     'No' : [1, 2, 3, 4,],
     'Nama' : ['KK', 'UU', 'OO', 'II'],
