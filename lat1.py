55754bf32ce294e713c26a2a8e2c59bed9cff43e
felieesya <rendrarim@gmail.co~>
2024-03-09 11:15:00 +0900

    v12

--- lat1.py
+++ lat1.py
@@ -3,6 +3,7 @@ from page1 import page_1
 from page2 import page_2
 from page3 import page_3
 from page4 import page_4
+from image1 import main
 #import pandas as pd
 #import matplotlib.pyplot as plt
 
@@ -21,7 +22,8 @@ PAGES = {
     "Page 1" : page_1,
     "Page 2" : page_2,
     "Page 3" : page_3,
-    "Hitung Luas" : page_4
+    "Hitung Luas" : page_4,
+    "Olah Gambar" : main
 }
 
 st.sidebar.image("bulat.png", width=200)
