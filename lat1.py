ea2650cb125405df95a66581999283520a7726ec
felieesya <rendrarim@gmail.co~>
2024-03-09 12:47:18 +0900

    r2

--- lat1.py
+++ lat1.py
@@ -30,23 +30,23 @@ st.sidebar.image("bulat.png", width=200)
 page = st.sidebar.radio("Halaman", list(PAGES.keys()))
 PAGES[page]()
 
-
-st.markdown(
-    """
-        <style>
-        [data-testid= "stActionButtonIcon"] {
-            display: none;
-        }
-        [data-testid= "baseButton-header"] {
-            display: none;
-        }
-        
-        #Mainmenu {visibility: hidden;}
-        footer {visibility: hidden;}
-        
-        </style>
-        """,
-    unsafe_allow_html= True,
-)
+def main():
+    st.markdown(
+        """
+            <style>
+            [data-testid= "stActionButtonIcon"] {
+                display: none;
+            }
+            [data-testid= "baseButton-header"] {
+                display: none;
+            }
+            
+            #Mainmenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            
+            </style>
+            """,
+        unsafe_allow_html= True,
+    )
 
 
